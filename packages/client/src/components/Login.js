74cb56efb72906662c24df2a2b19445669460705
martinbugayong <martinbugayong@gmail.com>
2019-10-08 22:22:34 -0700

    add keydown function

--- packages/client/src/components/Login.js
+++ packages/client/src/components/Login.js
@@ -1,63 +1,63 @@
-import React from 'react'
-import { useMutation, useApolloClient } from '@apollo/react-hooks'
-import { useState } from 'react'
+import React from 'react';
+import { useMutation, useApolloClient } from '@apollo/react-hooks';
+import { useState } from 'react';
 
-function Login({
-  buttonsDisplayed,
-  setButtonsDisplayed,
-  LOGIN
-}) {
+function Login({ buttonsDisplayed, setButtonsDisplayed, LOGIN }) {
   const client = useApolloClient();
-  const [login, { data, loading, error }] = useMutation(LOGIN,
-    {
-      onCompleted({ login }) {
-        localStorage.setItem('token', login.token)
-        client.writeData({ data: { isLoggedIn: true } })
-        console.log(login.token)
-      }
-    }
-  )
-  const [username, setUsername] = useState('')
-  const [password, setPassword] = useState('')
+  const [login, { data, loading, error }] = useMutation(LOGIN, {
+    onCompleted({ login }) {
+      localStorage.setItem('token', login.token);
+      client.writeData({ data: { isLoggedIn: true } });
+      console.log(login.token);
+    },
+  });
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
 
   function usernameOnChange(e) {
-    setUsername(e.target.value)
+    setUsername(e.target.value);
   }
 
   function passwordOnChange(e) {
-    setPassword(e.target.value)
+    setPassword(e.target.value);
   }
 
   function goBack() {
-    setButtonsDisplayed(true)
+    setButtonsDisplayed(true);
+  }
+
+  function keyDownLogin() {
+    login({ variables: { username: username, password: password } });
   }
 
   return (
     <div className="log-in__concept">
       <input
-        className='user-panel__input'
+        className="user-panel__input"
         placeholder="Username"
         onChange={usernameOnChange}
+        onKeyDown={keyDownLogin}
       />
       <input
-        type='password'
-        className='user-panel__input'
+        type="password"
+        className="user-panel__input"
         placeholder="Password"
         onChange={passwordOnChange}
+        onKeyDown={keyDownLogin}
       />
+      <button className="log-in-button" onClick={keyDownLogin}>
+        Log In
+      </button>
       <button
-        className='log-in-button'
+        className="back-button"
         onClick={() => {
-          login({ variables: { username: username, password: password } })
-        }
-        }>Log In</button>
-      <button
-        className='back-button'
-        onClick={() => {
-          goBack()
-        }}><i>&#60; Back</i></button>
+          goBack();
+        }}
+      >
+        <i>&#60; Back</i>
+      </button>
     </div>
-  )
+  );
 }
 
-export default Login
+export default Login;
