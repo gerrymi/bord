e78f80001a0123e49536725f8e0d84e84e5814bf
Pau Bonilla <paubonilla@Paus-MacBook-Pro.local>
2019-10-09 00:49:12 -0700

    Dashboard UI Update

--- packages/client/src/components/Dashboard.js
+++ packages/client/src/components/Dashboard.js
@@ -15,16 +15,31 @@ function Dashboard({
   // if (error) return <h1>{error.message}</h1>
   console.log(data)
   return (
-    <div className='nav-component'>
-      <div className='nav-link'>
+    <div className="dashboard-container">
+      <div className='nav-links'>
         <li className='nav-item'>Dashboard</li>
         <li className='nav-item'>How to Use Børd</li>
         <li className='nav-item'>Settings</li>
+
+        <button
+          className='logout-button'
+          onClick={() => localStorage.removeItem('token')}>
+          logout</button>
+
       </div>
-      <button
-        className='logout-button'
-        onClick={() => localStorage.removeItem('token')}>
-        logout</button>
+      <div className='dashboard-header'>
+        <div className='dashboard-header-text'>
+          <h1>Hey, Name!</h1>
+          <h5>You look good today! Here's what's on your agenda.</h5>
+        </div>
+      </div>
+      <div className='dashboard-panel'>
+        <div className='list-container'>
+          <div>List
+
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
